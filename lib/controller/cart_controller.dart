ee75a610b4921d85d005c866f5dbe11e6bed0024
chloemk <minsung.chloe.kim@gmail.com>
2022-08-01 00:23:14 +0900

    feat: add getter variables

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -8,6 +8,7 @@ class CartController extends GetxController {
   // EX cartItems가 초기화되지 않아 getter를 사용해야하는데
   // EX 초기화 된 값을 getter가 복사해서 가지고온다고 보면 된다.
   double get totalPrice => cartItems.fold(0, (prev, el) => prev + el.price);
+  int get count => cartItems.length;
 
   void addToItem(Product product) {
     cartItems.add(product);
